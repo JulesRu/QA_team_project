5dfeed26280309ab574ccf8cce7cb8fdb57734ac
Margarita <ktosik@inbox.ru>
2023-02-26 18:00:25 +0300

    Added Tests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -3,6 +3,8 @@ package ru.netology;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 public class PlayerTest {
 
     @Test
@@ -20,4 +22,46 @@ public class PlayerTest {
     }
 
     // другие ваши тесты
+
+    @Test
+    public void shouldSumGenreIfTwoGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Морской бой", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 3);
+        player.installGame(game2);
+        player.play(game2, 2);
+
+        int expected = 5;
+        int actual = player.sumGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldSumPlayTimeIfOneGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Kolya");
+        game.getStore().addPlayTime("Kolya", 2);
+
+        int expected = 7;
+        int actual = player.play(game, 5);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldSumPlayTimeIfOneGameIfInstallGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Kolya");
+        game.getStore().addPlayTime("Kolya", 2);
+        player.installGame(game);
+
+        int expected = 3;
+        int actual = player.play(game, 1);
+        assertEquals(expected, actual);
+    }
+
 }
